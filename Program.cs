d9352362c6c0492e01ce13321dfec9e81a40c5f2
Sam Dinh <xuansam@google.com>
2025-04-16 11:52:23 +0000

    SynthesisInput.Text

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Cloud.TextToSpeech.V1;
 using System;
+using System.IO;
 
 namespace TextToSpeechApiDemo
 {
@@ -8,11 +9,35 @@ namespace TextToSpeechApiDemo
         static void Main(string[] args)
         {
             var client = TextToSpeechClient.Create();
-            var response = client.ListVoices("vi");
-            foreach (var voice in response.Voices)
+
+            // The input to be synthesized, can be provided as text or SSML.
+            var input = new SynthesisInput
             {
-                Console.WriteLine($"{voice.Name} ({voice.SsmlGender}); Language codes: {string.Join(", ", voice.LanguageCodes)}");
+                Text = "Xin chào. Vui lòng cung cấp thông tin cá nhân, bao gồm Họ, Tên và Số điện thoại trước khi vào cổng trường."
+            };
+
+            // Build the voice request.
+            var voiceSelection = new VoiceSelectionParams
+            {
+                LanguageCode = "vi-VN",
+                SsmlGender = SsmlVoiceGender.Female
+            };
+
+            // Specify the type of audio file.
+            var audioConfig = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Mp3
+            };
+
+            // Perform the text-to-speech request.
+            var response = client.SynthesizeSpeech(input, voiceSelection, audioConfig);
+            
+            // Write the response to the output file.
+            using (var output = File.Create("output.mp3"))
+            {
+                response.AudioContent.WriteTo(output);
             }
+            Console.WriteLine("Audio content written to file \"output.mp3\"");
         }
     }
 }
